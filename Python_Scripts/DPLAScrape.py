8f9bc073e9c8a57cfcd5ed1b452f215da768ef4f
katiewolf57 <katielaurenwolf@gmail.com>
2018-11-15 12:31:59 -0500

    Update DPLAScrape
    adding in comments!

--- Python_Scripts/DPLAScrape.py
+++ Python_Scripts/DPLAScrape.py
@@ -17,12 +17,12 @@ dpla_all_data = []
 planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto']
 
 
-# looping through my planet list
-# page size is 500 and there are NOT more than 500 results for each planet --> don't have to loop through multiple pages
+## looping through my planet list
+## page size is 500 and there are NOT more than 500 results for each planet --> don't have to loop through multiple pages
 
 for planet in planets:
 
-	# making a directory to store files/images in
+	## making a directory to store files/images in
 	os.mkdir(planet)
 
 	print("Looking at " + planet + '!\n')
@@ -34,6 +34,7 @@ for planet in planets:
 	dpla_all_data.append(dpla_data)
 
 	## writing out the data for each planet it it's own JSON file for easier searching
+	## adding it to the planet folder
 	json.dump(dpla_data, open('./' + planet + '/' + planet +'.json', 'w'), indent=4)
 
 
@@ -56,46 +57,48 @@ images = []
 base = 'https://dp.la/thumb/'
 
 
-
 for planet in planets:
 
-	# for storing the thumbnails
-
+	## opening up the JSON file to search through for relevant info
 	f = open('./' + planet + '/' + planet + '.json', 'r')
 	planet_data = json.load(f)
 	planet_data = planet_data['docs']
 
-	# pulling the collection url for the item
+	## pulling the collection url for the item
 	for item in planet_data:
 		l_url = item['isShownAt']
 		object_urls.append(l_url)
 	
-	# pulling the title for the item
+	## pulling the title for the item
 	for item in planet_data:
 		l_title = item['sourceResource']['title']
 		titles.append(l_title)
 
 
-	# pulling the image id for the itme
+	## pulling the image id for the itme
 	for item in planet_data: 
 		image = item['id']
 		images.append(image)
 
-	# making a csv with the urls, titles, and image ids
+	## making a csv with the urls, titles, and image ids
+	## putting it in the planet folder
 	with open('./' + planet + '/' + planet + '.csv', 'w') as results:
 		wr = csv.writer(results)
 		wr.writerows(zip(object_urls, titles, images))
 
 
-
+	## writing images to the planet folder
 	for i in images:
+	
+	## making sure not to download an image if it's already been downloaded
 		if not os.path.isfile('./' + planet + '/' +i+'.jpg'):
 			r = requests.get(base + str(i))
 			if r.status_code == 200:
 				print("Downloading: ", i)
 				with open('./' + planet + '/'+ i + '.jpg', 'wb') as f:
 					f.write(r.content)
-
+					
+	## making sure that if any images aren't found/don't exist any more, it's reported so I can deal with them
 			else:
 				with open('error_log.txt', 'a') as f:
 					f.write("ERROR:" + i + '\n')
@@ -109,12 +112,24 @@ for planet in planets:
 	image_count = str(len(images))
 
 	print("\nThere should be " + title_count + " titles and " + url_count + " urls and  " + image_count + " images for " + planet)
-
-	if title_count != url_count:
-		print("WATCH OUT! Those numbers are different!\n")
+	
+	## alerting me if there's a discprepancy between title/url/image count
+	if title_count > url_count:
+		print("WATCH OUT! There are more titles than urls!\n")
+	elif title count > image_count:
+		print("WATCH OUT! There are more titles than images!\n")
+	elif url_count > title_count:
+		print("WATCH OUT! There are more urls than titles!\n")
+	elif url_count > image_count:
+		print("WATCH OUT! There are more urls than images!\n")
+	elif image_count > title_count:
+		print("WATCH OUT! There are more images than titles!\n")
+	elif image_count > url_count: 
+		print("WATCH OUT! There are more images than urls!\n")
 	else:
 		print("Those numbers match! All good!\n")
 
+	## clearing out the lists so they can be counted for the next planet
 	del object_urls[:]
 	del titles[:]
 	del images[:]
